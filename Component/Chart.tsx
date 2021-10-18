f3c62574ee9514422573fd32ccffcc1a1c0b2bb7
Mohsen <moh.1380.1393@gmail.com>
2021-10-18 12:50:17 +0330

    add api

--- Component/Chart.tsx
+++ Component/Chart.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useRef, useState } from "react";
 import { createChart, CrosshairMode } from "lightweight-charts";
-import { priceData } from "../Component/PriceData";
-import { volumeData } from "./volumeData";
 
-const Chart = () => {
+let candleSeries: any;
+let volumeSeries: any;
+const Chart = ({ data }: any) => {
+  //!------------------------
+  const [chartExist, setChartExist] = useState(false);
   const [size, setSize] = useState({
     x: window.innerWidth,
     y: window.innerHeight,
@@ -16,63 +18,98 @@ const Chart = () => {
   useEffect(() => (window.onresize = updateSize), []);
   const chartContainerRef: any = useRef();
   const chart: any = useRef();
+
   useEffect(() => {
-    chart.current = createChart(chartContainerRef.current, {
-      width: size.x,
-      height: size.y,
-      layout: {
-        backgroundColor: "#253248",
-        textColor: "rgba(255, 255, 255, 0.9)",
-      },
-      grid: {
-        vertLines: {
-          color: "#334158",
+    const PriceData = data.map((item: any) => {
+      const time = new Date(item[0]);
+      const KoreanTimeFormat = new Intl.DateTimeFormat("ko-KR").format(time);
+      let replaceDot = KoreanTimeFormat.replace(".", "-");
+      replaceDot = replaceDot.replace(".", "-");
+      const removeDot = replaceDot.replace(".", "");
+      const removeSpace = removeDot.replaceAll(" ", "");
+      return {
+        time: removeSpace,
+        open: parseFloat(item[1]),
+        high: parseFloat(item[2]),
+        low: parseFloat(item[3]),
+        close: parseFloat(item[4]),
+      };
+    });
+
+    const volumeData = data.map((item: any) => {
+      const time = new Date(item[0]);
+      const KoreanTimeFormat = new Intl.DateTimeFormat("ko-KR").format(time);
+      let replaceDot = KoreanTimeFormat.replace(".", "-");
+      replaceDot = replaceDot.replace(".", "-");
+      const removeDot = replaceDot.replace(".", "");
+      const removeSpace = removeDot.replaceAll(" ", "");
+      return {
+        time: removeSpace,
+        value: item[5],
+      };
+    });
+
+    if (!chartExist) {
+      chart.current = createChart(chartContainerRef.current, {
+        width: size.x,
+        height: size.y,
+        layout: {
+          backgroundColor: "#253248",
+          textColor: "rgba(255, 255, 255, 0.9)",
         },
-        horzLines: {
-          color: "#334158",
+        grid: {
+          vertLines: {
+            color: "#334158",
+          },
+          horzLines: {
+            color: "#334158",
+          },
         },
-      },
-      crosshair: {
-        mode: CrosshairMode.Normal,
-      },
+        crosshair: {
+          mode: CrosshairMode.Normal,
+        },
 
-      timeScale: {
-        borderColor: "#485c7b",
-      },
-    });
+        timeScale: {
+          borderColor: "#485c7b",
+        },
+      });
 
-    console.log(chart.current);
+      candleSeries = chart.current.addCandlestickSeries({
+        upColor: "#4bffb5",
+        downColor: "#ff4976",
+        borderDownColor: "#ff4976",
+        borderUpColor: "#4bffb5",
+        wickDownColor: "#838ca1",
+        wickUpColor: "#838ca1",
+      });
+      volumeSeries = chart.current.addHistogramSeries({
+        color: "rgba(31, 31, 31, 0.5)",
+        base: 2,
+        baseLineWidth: 2,
+        overlay: true,
+        priceFormat: {
+          type: "volume",
+        },
+        scaleMargins: {
+          top: 0.8,
+          bottom: 0,
+        },
+      });
+    }
 
-    const candleSeries = chart.current.addCandlestickSeries({
-      upColor: "#4bffb5",
-      downColor: "#ff4976",
-      borderDownColor: "#ff4976",
-      borderUpColor: "#4bffb5",
-      wickDownColor: "#838ca1",
-      wickUpColor: "#838ca1",
-    });
-
-    candleSeries.setData(priceData);
-
-    const volumeSeries = chart.current.addHistogramSeries({
-      color: "rgba(31, 31, 31, 0.5)",
-      base: 2,
-      baseLineWidth: 2,
-      overlay: true,
-      priceFormat: {
-        type: "volume",
-      },
-      scaleMargins: {
-        top: 0.8,
-        bottom: 0,
-      },
-    });
-
-    volumeSeries.setData(volumeData);
-  }, []);
+    if (!chartExist) {
+      setChartExist(true);
+      candleSeries.setData(PriceData);
+      volumeSeries.setData(volumeData);
+    } else {
+      candleSeries.update(PriceData[PriceData.length - 1]);
+      volumeSeries.update(volumeData[volumeData.length - 1]);
+    }
+  }, [data]);
   useEffect(() => {
     chart.current.resize(size.x - 18, size.y - 58);
   }, [size]);
+
   return (
     <>
       <div ref={chartContainerRef} className="chart-container" />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,92 @@
 import Head from "next/head";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
+
 const Chart = dynamic(() => import("../Component/Chart"), {
   ssr: false,
 });
-export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Mohsen_Bg</title>
-        <meta charSet="UTF-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Head>
 
-      <div style={{ marginTop: "20px", fontSize: "25px" }}>
-        Candle Static Chart
-      </div>
+export default function Home() {
+  const [data, setData] = useState<any>("");
+  const [ERROR, setError] = useState<any>(false);
+
+  const fetcher = async () => {
+    const limit = 300;
+    await fetch(
+      `https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1d&limit=${limit}`
+    )
+      .then(async (res) => {
+        const data = await res.json();
+        setData(data);
+      })
+      .catch((error) => {
+        setError(true);
+      });
+  };
+
+  const time = 1000;
+  useEffect(() => {
+    const interval = setInterval(async () => {
+      fetcher();
+    }, time);
+    return () => clearInterval(interval);
+  }, []);
+
+  if (!data && !ERROR) {
+    return (
       <div>
-        <Chart />
+        <Head>
+          <title>Loading</title>
+          <meta charSet="UTF-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+        </Head>
+        <div>Loading</div>
       </div>
-    </div>
+    );
+  }
+
+  return (
+    <>
+      {ERROR ? (
+        <div>
+          <Head>
+            <title>ERROR</title>
+            <meta charSet="UTF-8" />
+            <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1.0"
+            />
+          </Head>
+          <div>
+            Can Not Connect To The Api Binance If Your Country Ban By Binance
+            Use Vpn
+          </div>
+        </div>
+      ) : (
+        <div>
+          <Head>
+            <title>Chart (BTC-USDT) DAILY</title>
+            <meta charSet="UTF-8" />
+            <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1.0"
+            />
+          </Head>
+
+          <div style={{ marginTop: "20px", fontSize: "25px" }}>
+            Candle Static Chart (BTC-USDT) DAILY
+          </div>
+          <div>
+            <Chart data={data} />
+          </div>
+        </div>
+      )}
+    </>
   );
 }
